f16b9f440349096c5db7852687bb2095e3de42c8
Julien Breux <julienbreux@google.com>
2026-01-05 14:07:39 +0100

    feat: Improve spinner animation.

--- internal/run/ui/component/spinner/spinner.go
+++ internal/run/ui/component/spinner/spinner.go
@@ -1,24 +1,31 @@
 package spinner
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
-// New returns a simple "Loading..." text, right-aligned, with animation.
+var (
+	frames = []rune{
+		'⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏',
+	}
+	suffix = "Loading..."
+)
+
+// New returns a simple "⠋ Loading..." text, right-aligned, with animation.
 func New(app *tview.Application) tview.Primitive {
 	textView := tview.NewTextView().
 		SetTextColor(tcell.ColorWhite).
 		SetTextAlign(tview.AlignRight)
 
 	go func() {
-		frames := []string{"Loading .  ", "Loading .. ", "Loading ..."}
 		i := 0
 		for {
 			app.QueueUpdateDraw(func() {
-				textView.SetText(frames[i])
+				textView.SetText(fmt.Sprintf("%s %s", string(frames[i]), suffix))
 			})
 			i = (i + 1) % len(frames)
 			time.Sleep(500 * time.Millisecond)
